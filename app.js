b964b7b484010af2c6683e422571a1d32fc16a56
Xerdet <lurobper@gmail.com>
2019-01-25 16:52:01 -0600

    Uploaded final app.js with full documentation

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+//Creates initial global variables that will be used throughout the functions
 var ingredientsArray = [];
 var foodCity;
 var foodType;
+var googleAPI="AIzaSyCQyeLt-vLwx27BDKm319Ls0Pn1dAvmThk";
 
+//Initializes document before the JS begins running
 $(document).ready(function () {
-  // initializes dropdown
+
+  // Initializes dropdown menu
   $('select').formSelect();
 
-  //hides submit buttons, dine in, and pickup options on page load
+  //Hides submit buttons, Dine In, and Pickup options upon the page loading
   $("#restaraunt").hide();
   $("#foodInputs").hide();
   $("#foodInputsSubmit").hide();
@@ -17,49 +21,61 @@ $(document).ready(function () {
 
   //Function that takes the response from the AJAX request and separates it into the necessary elements in order to create a card for each recipe.
   function getRecipes(x) {
+
     //Clears any previous content where the Recipes will be displayed
     $("#recipeList").empty();
     $("#recipeList2").empty();
-    var mealResults = x.hits;
-    //Cycles through all the results to separate them into their own cards. 
+    $("#recipeList3").empty();
 
+    //Assigns the response results to a variable to be used by the function
+    var mealResults = x.hits;
+
+    //Cycles through all the results to separate them into their own columns. 
     for (i = 0; i < mealResults.length; i++) {
       if (i == 0 || i == 5) {
         var rCol = $("<div>").addClass("col s2 offset-s1");
       } else {
         var rCol = $("<div>").addClass("col s2");
       }
-      //Adds a link to the Full Recipe on the image of the result
+    //Adds a link to the Full Recipe on the image of the result
       var rImg = $("<img>").attr("src", mealResults[i].recipe.image);
       var linkUrl = mealResults[i].recipe.url;
       var rLink = $("<a>").attr("href", linkUrl).attr("target", "_blank");
       (rLink).append(rImg);
       var imgDiv = $("<div>").addClass("card-image").append(rLink);
+
+    //Creates the contents of the card. Preparing the Title of the Recipe and passing the ingredient list to the proper function before appending the resulting list.
       var rTitle = $("<div>").addClass("card-title center pd10").text(mealResults[i].recipe.label)
       var rIng = mealResults[i].recipe.ingredientLines;
       var rlist = ingredientList(rIng);
       var rCard = $("<div>").addClass("card grey lighten-4 left")
-
+    
+    //Appends each card with the Image, Title and Ingredient list before appending it to the proper column
       rCard.append(imgDiv, rTitle, rlist);
       rCol.append(rCard);
-      if (i <= 4) {
 
+    //Separates the columns into groups of 5 for aesthetic purposes
+      if (i <= 4) {
         $("#recipeList").append(rCol);
       } else {
         $("#recipeList2").append(rCol);
       }
     }
   };
-  // sets foodType variable to dropdown menu selection
+
+  // Event Listener that will initiate the Restaurant Search API function upon the user clicking Submit
   $("#submitTer").on("click", function (event) {
     event.preventDefault();
 
+  //Grabs the user's input and assigns them to the variables that will be used for the rest of the process.
     foodType = $("#dropDown").val().trim();
     foodCity = $("#cityName").val().trim();
 
+  //Prepares the Query URL for the first AJAX call
    var apiKey = "273331ea460eeca63bfcf2af46d9a0c9";
    var cityURL = "https://developers.zomato.com/api/v2.1/cities?q=" + foodCity;
-   
+  
+  //Makes an AJAX call to the Zomato API to get the requested city ID in order to then make the Restaurant Search
     $.ajax({
       url: cityURL,
       beforeSend: function(xhr){xhr.setRequestHeader('user-key', 
@@ -67,13 +83,14 @@ $(document).ready(function () {
       method: "GET"
       
     }).then(function (response) {
-      console.log(response);
+    
+    //After the initial response is returned, then the function makes a second API call in order to search for the requested restaurant type in that city
       getRestaurants(response, foodType);
   
     })
   });
 
-
+//Function that shows the restaurant search options and hides the recipe search options
   $("#pickUp").on("click", function () {
     $("#restaraunt").show();
     $("#foodInputs").hide();
@@ -83,9 +100,11 @@ $(document).ready(function () {
     $("#pickUpSubmit").show()
     $("#recipeList").empty();
     $("#recipeList2").empty();
+    $("#recipeList3").empty();
     $("footer").addClass("footerStart")
   })
 
+//Function that shows the restaurant search options and hides the recipe search options  
     $("#foodD").on("click", function () {
     $("#foodInputs").show();
     $("#restaraunt").hide();
@@ -93,10 +112,13 @@ $(document).ready(function () {
     $("#using").show();
     $("#foodInputsSubmit").show();
     $("#pickUpSubmit").hide()
+    $("#recipeList").empty();
+    $("#recipeList2").empty();
+    $("#recipeList3").empty();
     $("footer").addClass("footerStart")
   })
 
-  //Function that takes the input from the "Ingredients" form and separates them, trims them and returns a variable to be inserted into the Query URL
+//Function that takes the input from the "Ingredients" form and separates them, trims them and returns a variable to be inserted into the Query URL
   function ingSearch(p) {
     var separateIngs = p.split(",");
     var ingURL = "";
@@ -109,9 +131,9 @@ $(document).ready(function () {
     }
     return ingURL;
 
-  }
+  };
 
-  //Function that takes the input from the "Excluded" form and separates them, trims them and returns a variable to be inserted into the Query URL
+//Function that takes the input from the "Excluded" form and separates them, trims them and returns a variable to be inserted into the Query URL
   function exclSearch(q) {
     var separateExcl = q.split(",");
     var exclURL = "";
@@ -124,9 +146,9 @@ $(document).ready(function () {
     }
     return exclURL;
 
-  }
+  };
 
-  //Creates the Ingredients List based on the input for each recipe
+//Creates the Ingredients List based on the input for each recipe
   function ingredientList(a) {
 
     //Creates a div where all the ingredients will go
@@ -138,11 +160,17 @@ $(document).ready(function () {
       $(recipeText).append(ingText);
     }
     return recipeText;
-  }
+  };
+
+//Function that uses the City ID and the requested cuisine type in order to create an AJAX call to the Zomato API
   function getRestaurants(o,p) {
+  
+  //Obtains the first city id from the response, then combines that and the cuisine value to create the Query URL
     var cityID = o.location_suggestions[0].id;
     var resURL = "https://developers.zomato.com/api/v2.1/search?entity_id=" + cityID + "&entity_type=city" + "&cuisines=" + p + "&count=9";
     var apiKey = "273331ea460eeca63bfcf2af46d9a0c9";
+
+  //Makes an AJAX call to the Zomato API in order to get the restaurant list for that city
     $.ajax({
       url: resURL,
       beforeSend: function(xhr){xhr.setRequestHeader('user-key', 
@@ -150,62 +178,78 @@ $(document).ready(function () {
       method: "GET"
       
     }).then(function (response) {
-      console.log(response);
+      
+    //Waits for the response from the query to be returned before using it to call the Restaurant List function
       restList(response);
     })
 
   };
 
+//Function that uses the response from the restaurant AJAX call to dynamically populate the restaurant list
   function restList(k)
   {
-     //Clears any previous content where the Recipes will be displayed
+     //Clears any previous content in the area where the Restaurants will be displayed
      $("#recipeList").empty();
      $("#recipeList2").empty();
      $("#recipeList3").empty();
+
+    //Assigns the Restaurants results to a variable that the function will use throughout the entire run
      var restResults = k.restaurants;
-     console.log(restResults);
-     //Cycles through all the results to separate them into their own cards. 
- 
-     for (var i = 0; i < 9; i++) {
-      //  if (i == 0) {
-      //    var rCol = $("<div>").addClass("col s3 offset-s1");
-      //  } else if (i==3 || i == 5) {
-      //    var rCol = $("<div>").addClass("col s3 offset-s1");
-      //  } else if(i==6 || i==8){
-      //   var rCol = $("<div>").addClass("col s3 offset-s1");
-      //  }else{
-        var rCol = $("<div>").addClass("col s3 offset-s1");
-      //  }
+    
+    //Cycles through all the results to create a column for each of their cards
+    for (var i = 0; i < 9; i++) {
+      
+      var rCol = $("<div>").addClass("col s3 offset-s1");
+   
+      //Grabs the Restaurant name from the result array in the respective position
+      var restName = restResults[i].restaurant.name;
+      
+      //Creates a list item with the Average User Review
+      var restReview = $("<li>").text("Average User Review: " + restResults[i].restaurant.user_rating.aggregate_rating + "/5");
+      
+      //Creates the Price Range section before it will be appended
+      var pRange = restResults[i].restaurant.price_range;
+      var restRange = "";
        
-       var restName = restResults[i].restaurant.name;
-       
-       var restReview = $("<li>").text("Average User Review: " + restResults[i].restaurant.user_rating.aggregate_rating + "/5");
+      for(var x=1; x<=pRange; x++)
+      {
+       restRange = restRange + "$";
+      }
+      
+      //The rest of the list elements are created before being appended to the card info section
+       var costRange = $("<li>").text("Price Range: " + restRange);
        var avCost = $("<li>").text("Average Cost for Two: $" + restResults[i].restaurant.average_cost_for_two);
        var restLocation = $("<li>").text("Address: " + restResults[i].restaurant.location.address + "," + restResults[i].restaurant.location.city + "," + restResults[i].restaurant.location.zipcode);
-       var infoList = $("<ul>").append(restReview, avCost, restLocation);
+       var menuUrl = restResults[i].restaurant.menu_url;
+       var restCuisines = $("<li>").text("Cuisines: " + restResults[i].restaurant.cuisines);
+       var rLink = $("<a>").attr({"href": menuUrl,"target":"_blank"}).text("See the Menu!");
+       var menuLink = $("<li>").append(rLink);
+       var infoList = $("<ul>").append(restReview, restCuisines, costRange, avCost, restLocation, menuLink);
        var restImg = restResults[i].restaurant.featured_image;
 
-       var rCard = $("<div>").addClass("card left liHeight");
-
+       var rCard = $("<div>").addClass("card left");
+      
+      //Checks if the restaurant info has a preview image. If not then it sets a placeholder image
        if(restImg != ""){
         var rImg = $("<img>").addClass("activator").attr("src", restImg);
        }else{
         var rImg = $("<img>").addClass("activator").attr("src", "food-placeholder.jpg");
        };
-
+      
+      //Creates the different sections of each card before appending them all together
        var imgDiv = $("<div>").addClass("card-image waves-effect waves-block waves-light").append(rImg);
        var rTitle = $("<span>").addClass("card-title activator center pd10").text(restName);
-       var rContent = $("<div>").addClass("card-content").append(rTitle);
-       rTitle.append(infoList);
-       var rReveal = $("<div>").addClass("card-reveal").append(rTitle);
-
-       var menuUrl = restResults[i].menu_url;
-       var rLink = $("<a>").attr("href", menuUrl).attr("target", "_blank");
-    
-       rCard.append(imgDiv, rContent, rReveal);
+       var rTitle2 = $("<span>").addClass("card-title activator center pd10").text(restName);
+       var rContent = $("<div>").addClass("card-content").append(rTitle2);
+       rTitle2.append(infoList);
+       var rReveal = $("<div>").addClass("card-reveal").append(rTitle2);
+      
+      //Puts together the card sections before appending the completed card to its respective column
+       rCard.append(imgDiv, rTitle, rContent, rReveal);
        rCol.append(rCard);
-       if (i <= 2) {
- 
+      
+      //Separates the columns into 3 rows for aesthetic reasons
+      if (i <= 2) {
          $("#recipeList").append(rCol);
        } else if(i<=5) {
          $("#recipeList2").append(rCol);
@@ -236,7 +280,6 @@ $(document).ready(function () {
     //Verifies if the "Excluded" field is empty or not. If it is then it just searches for ingredients. If it isn't empty then it adds the "excluded" ingredients to the search
     if (exclude != "") {
       foodURL = "https://api.edamam.com/search?" + id + "&" + key + "&" + ingredient + "&" + "excluded=" + exclude;
-      console.log(foodURL);
     } else {
       foodURL = "https://api.edamam.com/search?" + id + "&" + key + "&" + ingredient;
     }
